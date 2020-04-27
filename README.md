8737c358e0d600342b43f8cfa129a7fb0676a206
Artur Yumaev <art.yumaev@gmail.com>
2020-04-28 01:21:41 +0300

    minimally working version

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # avito-antibot-developer-trainee
+
+```shell
+pip3 install flask
+sudo apt install python3-flask
+```
+
+run redis
+
+```shell
+src/redis-server
+```
+
+
--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-from flask import Flask, request
-app = Flask(__name__)
+import redis
+from flask import Flask, request, abort
 
-ipBucket = {}
+app = Flask(__name__)
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+
+def responseAllowed(token, timeout=15, nRequests=3):
+    # Check the lock for subnet
+    if r.get(token + '_LOCK'):
+        return False
+    else:
+        with r.pipeline() as pipe:
+            pipe.multi()
+            # Issue expired token
+            pipe.set(name=token, value=0, ex=timeout, nx=True)
+            pipe.incr(token)
+            pipe.execute()
+
+        # If the number of requests has not exceeded the limit,
+        # allow the connection
+        if r.get(token) != None and int((r.get(token)).decode('utf-8')) < nRequests:
+            return True
+        else:
+            # Set the lock for 2 minutes
+            r.set(name=token + '_LOCK', value=0, ex=120, nx=True)
+            return False
+
 
 def getIp(request):
     if request.headers.getlist("X-Forwarded-For"):
@@ -17,4 +41,7 @@ def home():
     ip = getIp(request)
     subNet = ip[:ip.rfind('.')]
 
-    return 'Home'
+    if responseAllowed(subNet):
+        return ip
+    else:
+        abort(429)
